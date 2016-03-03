3a53a60a330b25a594c8ea6f7cb3c54f75c35f8e
Juan Oliva <alu0100502923@ull.edu.es>
2016-03-03 01:23:30 +0000

    añadida expresion regular y corrigiendo/mejorando cosas

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -63,7 +63,7 @@
   Celsius.prototype = new Temperatura();
   Celsius.prototype.constructor = Celsius;
 
-  function Farenheit(valor)
+  function Fahrenheit(valor)
   {
     Temperatura.call(this, valor, "Fahrenheit");
   }
@@ -81,38 +81,65 @@
 
   exports.Temperatura = Temperatura;
   exports.Celsius = Celsius;
-  exports.Farenheit = Farenheit;
+  exports.Fahrenheit = Fahrenheit;
   exports.Kelvin = Kelvin;
 
   exports.convertir = function() {
-    var valor     = document.getElementById('convert').value,
-        elemento  = document.getElementById('converted'),
-        /* Extienda la RegeExp a la especificación. use una XRegExp */
-        regexp    = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([a-z,A-Z]+)\s*$/i;
-    valor     = valor.match(regexp);
+   var valor = document.getElementById('convert').value,
+     elemento = document.getElementById('converted'),
+     tipos_aceptados = ["c", "f", "k"],
+     regexp = XRegExp(
+       '(?<numero>    [+-]?\\d+(\\.\\d+)?([e][+-]?\\d+)?)# valor  \n\
+        (?<tipo>       [a-z]+)[ ]+(?:to[ ]+)?\
+        (?<tipo2> [a-z]+)[ ]*$', 'xi'),
+     valor = XRegExp.exec(valor, regexp);
 
-    if (valor) {
-      var numero = valor[1],
-          tipo   = valor[2].toLowerCase();
+   if (valor) {
+     var numero = valor.numero.replace(/\s+/g, ''),
+       tipo = valor.tipo.toLowerCase(),
+       tipo2 = valor.tipo2.toLowerCase();
 
-      numero = parseFloat(numero);
-      console.log("Valor: " + numero + ", Tipo: " + tipo);
+     if (tipos_aceptados.indexOf(tipo) > -1 && tipos_aceptados.indexOf(tipo2) > -1) {
+       elemento.style.color = "rgb(17, 5, 169)";
+       console.log("Valor: " + numero + ", Tipo: " + tipo + ", Nuevo: " + tipo2);
+       numero = parseFloat(numero);
+       var inicial;
+       switch (tipo) {
+         case 'c':
+           inicial = new Celsius(numero);
+           break;
+         case 'f':
+           inicial = new Fahrenheit(numero);
+           break;
+         case 'k':
+           inicial = new Kelvin(numero);
+           break;
+         default:
+           console.log("No hay asignado un case para este valor");
+           break;
+       }
 
-      switch (tipo) {
-        case 'c':
-          var celsius = new Celsius(numero);
-          elemento.innerHTML = celsius.toFarenheit().toFixed(2) + " Farenheit";
-          break;
-        case 'f':
-          var farenheit = new Farenheit(numero);
-          elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " Celsius";
-          break;
-
-        default:
-          /* rellene este código */
-      }
-    }
-    else
-      elemento.innerHTML = "";
-  };
+       switch (tipo2) {
+         case 'c':
+           elemento.innerHTML = inicial.toCelsius().toFixed(2) + " Celsius";
+           break;
+         case 'f':
+           elemento.innerHTML = inicial.toFahrenheit().toFixed(2) + " Fahrenheit";
+           break;
+         case 'k':
+           elemento.innerHTML = inicial.toKelvin().toFixed(2) + " Kelvin";
+           break;
+         default:
+           console.log("No hay asignado un case para este valor");
+           break;
+       }
+     } else {
+       elemento.style.color = "rgb(138, 0, 0)";
+       elemento.innerHTML = "ERROR. Introduzca por ejemplo -32.5e10f to K";
+     }
+   } else {
+     elemento.style.color = "rgb(138, 0, 0)";
+     elemento.innerHTML = "ERROR. Introduzca por ejemplo -32.5e10f to K";
+   }
+ }
 })(this);
